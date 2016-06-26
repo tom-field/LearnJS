990c558527f6f289fce6a63ed44aa265837aa720
tom-field <791025196@qq.com>
2016-06-26 23:05:03 +0800

    今天只官网plunker优化成功

--- ui-router/官网plunker优化/app.js
+++ ui-router/官网plunker优化/app.js
@@ -1,36 +1,4 @@
 /**
  * Created by Administrator on 2016/6/16.
  */
-var myapp = angular.module('myapp', ["ui.router","firstContrl","secondContrl"])
-myapp.config(function($stateProvider, $urlRouterProvider){
-
-    // For any unmatched url, send to /route1
-    $urlRouterProvider.otherwise("/route1")
-
-    $stateProvider
-        .state('route1', {
-            url: "/route1",
-            templateUrl: "./partial1/route1.html"
-        })
-        .state('route1.list', {
-            url: "/list",
-            templateUrl: "./partial1/route1.list.html",
-            controller: "firstController"
-            /*function($scope){
-                $scope.items = ["A", "List", "Of", "Items"];
-            }*/
-        })
-
-        .state('route2', {
-            url: "/route2",
-            templateUrl: "./partial2/route2.html"
-        })
-        .state('route2.list', {
-            url: "/list",
-            templateUrl: "./partial2/route2.list.html",
-            controller: "secondController"
-            /*function($scope){
-                $scope.things = ["A", "Set", "Of", "Things"];
-            }*/
-        })
-})
+var myapp = angular.module('myapp', ["myrouter","firstContrl","secondContrl"])
