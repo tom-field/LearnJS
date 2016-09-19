b2f237cc69ff7037eb94a90a1d8ca92f300d4c01
tom-field <791025196@qq.com>
2016-09-19 20:14:15 +0800

    基本指令

--- 09UI-Bootstrap/01ui.bootstrap.typeahead/example.js
+++ 09UI-Bootstrap/01ui.bootstrap.typeahead/example.js
@@ -1,3 +1,17 @@
-/**
- * Created by Administrator on 2016/9/12.
- */
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope, $http) {
+
+    $scope.getLocation = function(val) {
+        return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+                address: val,
+                sensor: false
+            }
+        }).then(function(response){
+            return response.data.results.map(function(item){
+                return item.formatted_address;
+            });
+        });
+    };
+
+});
--- 09UI-Bootstrap/02ng-Modal/01基本示例/controller.js
+++ 09UI-Bootstrap/02ng-Modal/01基本示例/controller.js
@@ -1,3 +1,36 @@
-/**
- * Created by Administrator on 2016/9/19.
- */
+angular.module('myApp',['ui.bootstrap']).controller('modalDemo',function($scope,$modal,$log){ //
+    $scope.items = ['html5','jq','FE-演示平台'];
+    $scope.open = function(size){  //打开模态
+        var modalInstance = $modal.open({
+            templateUrl : 'myModelContent.html',  //指向上面创建的视图
+            controller : 'ModalInstanceCtrl',// 初始化模态范围
+            size : size, //大小配置
+            resolve : {
+                itemsss : function(){
+                    return $scope.items;
+                },
+                hahahah: function () {
+                    return {'name':'xuhui'}
+                }
+            }
+        })
+        modalInstance.result.then(function(selectedItem){
+            $scope.selected = selectedItem;
+        },function(){
+            $log.info('Modal dismissed at: ' + new Date())
+        })
+    }
+})
+angular.module('myApp').controller('ModalInstanceCtrl',function($scope,$modalInstance,itemsss,hahahah){ //依赖于modalInstance
+    $scope.items = itemsss;
+    console.log(hahahah);
+    $scope.selected = {
+        item : $scope.items[0]
+    };
+    $scope.ok = function(){
+        $modalInstance.close($scope.selected.item); //关闭并返回当前选项
+    };
+    $scope.cancel = function(){
+        $modalInstance.dismiss('cancel'); // 退出
+    }
+})
