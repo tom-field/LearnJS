07937bdff39c592c474493bb792d5287789d39fe
tom-field <791025196@qq.com>
2016-11-27 02:17:23 +0800

    配置静态文件中间件

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require("express");
 
 const app = express();
 
-app.use(require("./router"))
+//配置静态文件服务中间件
+app.use('/www', express.static('www'));
+
+//加载路由中间件
+app.use(require("./router"));
 
 app.listen("3000","127.0.0.1", function () {
     console.log("服务正在运行");
