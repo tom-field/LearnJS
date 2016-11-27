ec0b853a6af2764ba5d5c4a6f45af182e80eff47
tom-field <791025196@qq.com>
2016-11-27 13:56:57 +0800

    处理请求体的中间件

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
 
 //配置静态文件服务中间件
 app.use('/www', express.static('www'));
+
+//配置解析post请求体的中间件
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//配置模板引擎
+app.set('views','./views');
+app.set('view engine', 'xtpl');
+app.use(function(req, res){
+    res.render('test',{data:1});
+});
 
 //加载路由中间件
 app.use(require("./router"));
