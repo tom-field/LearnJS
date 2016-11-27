0e41b6a3667194f088171b03bc6fe86d70a5a643
tom-field <791025196@qq.com>
2016-11-27 14:18:53 +0800

    1.xtpldemo添加注册页面 2.加载模板文件以及静态文件

--- demo/xtpl-demo.js
+++ demo/xtpl-demo.js
@@ -1,6 +1,16 @@
 var xtpl = require('xtpl');
+//渲染登录页面
 xtpl.renderFile('./login.xtpl', {
   title: '我是登录页面'
+}, function (error, content) {
+  if (error) {
+    return console.log('读取文件失败');
+  }
+  console.log(content);
+});
+//渲染注册页面
+xtpl.renderFile('./login.xtpl', {
+  title: '我是注册页面'
 }, function (error, content) {
   if (error) {
     return console.log('读取文件失败');
