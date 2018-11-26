cc520e6a429a04512b020674726be7889b5305af
xuhui <xuhui@juzix.io>
2018-11-26 13:49:43 +0800

    test

--- test/app/service/topic.test.js
+++ test/app/service/topic.test.js
@@ -4,7 +4,6 @@ describe('test/app/service/topic.test.js', () => {
     describe('testPopulate', () => {
         it('should ok', async () => {
             const ctx = app.mockContext();
-            console.log(ctx);
             const [topic] = await ctx.service.topic.getTopicById('5bf7a5b2efc9d82334247a1f');
             assert(topic.id === '5bf7a5b2efc9d82334247a1f');
         });
